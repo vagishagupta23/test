df3ce34d84a33b3281e7a40e4ff561b6cb345a86
Vagisha Gupta <vagishagupta23@gmail.com>
2017-03-28 02:34:35 +0530

    Add files via upload

--- check.py
+++ check.py
@@ -1,15 +1,18 @@
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
-@app.route('/')
-def main():
-    return render_template('heyaa.html')
+@app.route('/', methods=['POST','GET'])
+def temp():
+    if request.method == 'GET':
+        return render_template('heyaa.html')
+    
 
 @app.route('/result', methods=['POST','GET'])
 def hello():
     ls=['amit','rishab','prateek','rahul'];
     if request.method == 'POST':
-         return render_template('check.html', name=ls)
+        return render_template('check.html', name=ls)
+    
 if __name__ == '__main__':
    app.run(debug=True)
         
